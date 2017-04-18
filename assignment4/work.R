5ff667d662a5bb61d71112a9bedc53ab64ea140f
Robert Blake <rblake@mail.umw.edu>
2017-04-18 13:55:12 -0400

    Add some more to the work file

--- assignment4/work.R
+++ assignment4/work.R
@@ -7,3 +7,42 @@
 
 # Load up the salaries data
 sal <- read.csv("/home/robertblake/Projects/401/assignment4/Salaries.csv")
+
+# Plot basic salaries verses time colored for male and female
+ggplot(sal, aes(yrs.service,salary)) + geom_point(aes(color = factor(sex)))
+
+
+
+#####
+##### 2
+#####
+
+# Grab some avgs. for each rank's years of service
+mean(sal[sal['rank'] == 'Prof',]$yrs.service)
+mean(sal[sal['rank'] == 'AsstProf',]$yrs.service)
+mean(sal[sal['rank'] == 'AssocProf',]$yrs.service)
+
+# basic plot yrs.service vs. salary, colored by rank
+ggplot(sal,aes(yrs.service,salary)) + geom_point(aes(color = factor(rank)))
+
+
+
+
+#####
+##### 3
+#####
+
+# Just looking at both disciplines plotted
+ggplot(sal,aes(yrs.service,salary)) + geom_point(aes(color = factor(discipline)))
+
+
+# Separating the disciplines
+sal.a <- sal[sal['discipline'] == 'A',]
+sal.b <- sal[sal['discipline'] == 'B',]
+
+
+# Plot a with a trendline
+ggplot(sal.a,aes(yrs.service,salary)) + geom_point() + geom_smooth(model=lm)
+
+# Plot b with a trendline
+ggplot(sal.b,aes(yrs.service,salary)) + geom_point() + geom_smooth(model=lm)
